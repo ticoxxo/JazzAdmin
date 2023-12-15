00cc65728209a088964b0cd6b624b50a0687c2a5
ticox <ticox@TICOXXOPC>
2023-12-15 11:12:13 -0700

    Agregar archivos de proyecto.

---
+++ LogInForm.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace JazzAdmin
+{
+    partial class LogInForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogInForm));
+            this.pictureLogo = new System.Windows.Forms.PictureBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.usersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.test_jazzclubDataSet = new JazzAdmin.test_jazzclubDataSet();
+            this.usersTableAdapter = new JazzAdmin.test_jazzclubDataSetTableAdapters.usersTableAdapter();
+            this.btnForgotPass = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.test_jazzclubDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureLogo
+            // 
+            this.pictureLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(42)))), ((int)(((byte)(36)))));
+            this.pictureLogo.Image = ((System.Drawing.Image)(resources.GetObject("pictureLogo.Image")));
+            this.pictureLogo.Location = new System.Drawing.Point(-4, -1);
+            this.pictureLogo.Name = "pictureLogo";
+            this.pictureLogo.Size = new System.Drawing.Size(355, 454);
+            this.pictureLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureLogo.TabIndex = 0;
+            this.pictureLogo.TabStop = false;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.Location = new System.Drawing.Point(407, 94);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(47, 20);
+            this.lblUser.TabIndex = 1;
+            this.lblUser.Text = "User";
+            // 
+            // txtUser
+            // 
+            this.txtUser.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.usersBindingSource, "name", true));
+            this.txtUser.Location = new System.Drawing.Point(501, 94);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(209, 20);
+            this.txtUser.TabIndex = 2;
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.Location = new System.Drawing.Point(407, 161);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(86, 20);
+            this.lblPass.TabIndex = 3;
+            this.lblPass.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.usersBindingSource, "pass", true));
+            this.txtPassword.Location = new System.Drawing.Point(501, 161);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(209, 20);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(501, 256);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(87, 50);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Entrar";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(623, 256);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(87, 50);
+            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // usersBindingSource
+            // 
+            this.usersBindingSource.DataMember = "users";
+            this.usersBindingSource.DataSource = this.test_jazzclubDataSet;
+            // 
+            // test_jazzclubDataSet
+            // 
+            this.test_jazzclubDataSet.DataSetName = "test_jazzclubDataSet";
+            this.test_jazzclubDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // usersTableAdapter
+            // 
+            this.usersTableAdapter.ClearBeforeFill = true;
+            // 
+            // btnForgotPass
+            // 
+            this.btnForgotPass.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnForgotPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForgotPass.Location = new System.Drawing.Point(634, 388);
+            this.btnForgotPass.Name = "btnForgotPass";
+            this.btnForgotPass.Size = new System.Drawing.Size(143, 50);
+            this.btnForgotPass.TabIndex = 7;
+            this.btnForgotPass.Text = "Olvidé password";
+            this.btnForgotPass.UseVisualStyleBackColor = false;
+            this.btnForgotPass.Click += new System.EventHandler(this.btnForgotPass_Click);
+            // 
+            // LogInForm
+            // 
+            this.AcceptButton = this.btnLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnForgotPass);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.pictureLogo);
+            this.Name = "LogInForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login ";
+            this.Load += new System.EventHandler(this.LogInForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.test_jazzclubDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureLogo;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnCancelar;
+        private test_jazzclubDataSet test_jazzclubDataSet;
+        private System.Windows.Forms.BindingSource usersBindingSource;
+        private test_jazzclubDataSetTableAdapters.usersTableAdapter usersTableAdapter;
+        private System.Windows.Forms.Button btnForgotPass;
+    }
+}
+
---
+++ LogInForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JazzAdmin.test_jazzclubDataSetTableAdapters;
+
+namespace JazzAdmin
+{
+    public partial class LogInForm : Form
+    {
+        public LogInForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            HomeForm homeForm = new HomeForm();
+            usersTableAdapter.ClearBeforeFill = true;
+            usersTableAdapter.FillByNameAndPass(test_jazzclubDataSet.users, txtUser.Text, txtPassword.Text);
+            if(test_jazzclubDataSet.users.Count == 0)
+            {
+                MessageBox.Show("No matches user and pass");
+                txtUser.Clear();
+                txtUser.Focus();
+                txtPassword.Clear();
+            } else
+            {
+                this.Hide();
+                homeForm.ShowDialog();
+                this.Close();
+            }
+        }
+
+        private void LogInForm_Load(object sender, EventArgs e) {}
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnForgotPass_Click(object sender, EventArgs e)
+        {
+            string passWord;
+            usersTableAdapter.ClearBeforeFill = true;
+            passWord = usersTableAdapter.GetPassWordQuery(txtUser.Text);
+            if (passWord != String.Empty || passWord != null) { 
+                MessageBox.Show("The password esssss : " + passWord);
+            }
+            else { 
+                MessageBox.Show("No matched pass");
+            }
+        }
+    }
+}
